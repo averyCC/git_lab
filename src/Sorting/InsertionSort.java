fc5a7351ea17dabc18f9c10192f483aaab8776cf
aquablib <jingjing_cao@brown.edu>
2016-03-18 15:41:28 -0400

    committing insertion sort

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -4,12 +4,23 @@ public class InsertionSort {
 	/**
 	 * insertionSort			sorts an array of integers using insertion sort
 	 * 
-	 * @param unsortedArray		an unsorted array of integers
+	 * @param num		an unsorted array of integers
 	 * @return					the input array, sorted least to greatest
 	 */
-	public static int[] insertionSort(int[] unsortedArray) {
-		//TODO: S2 implements insertion sort
-		
-		return null;
-	}
+	public static void InsertionSort( int [ ] num)
+{
+     int j;        
+     int key;    
+     int i;  
+
+     for (j = 1; j < num.length; j++)   
+    {
+           key = num[ j ];
+           for(i = j - 1; (i >= 0) && (num[ i ] < key); i--)   
+          {
+                 num[ i+1 ] = num[ i ];
+          }
+         num[ i+1 ] = key;    
+     }
+}
 }
